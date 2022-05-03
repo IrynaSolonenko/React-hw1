57ba34da0f924231b7efb5e3135ecca136d1be5d
IrynaSolonenko <iryna17z89@gmail.com>
2022-05-03 20:34:43 +0300

    commit react-hw1

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export function Footer(){
+    return(
+        <footer>
+            <div>
+                <p>Privacy Police</p>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+export function Header(){
+    return(
+        <header>
+            <h1>Welcome</h1>
+            <menu>
+                <li>home</li>
+                <li>contacts</li>
+                <li>about</li>
+            </menu>
+        </header>
+    )
+}
